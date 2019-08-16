03f88e494aba46f480ccbae4db2ce809f13dc1ed
Adri Gispen <aegispen@gmail.com>
2019-08-17 01:19:38 +0200

    implemented assessment class

--- src/assessment.js
+++ src/assessment.js
@@ -1,5 +1,53 @@
-class Assessment {
+import Dimensions from "./dimensions";
 
+class Assessment {
+  constructor() {
+    this.questions = this.createQuestions();
+    this.currentQuestion = -1;
+    this.result = {};
+  }
+
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  createQuestions() {
+    let questions = [];
+    let shuffled = Dimensions.map(el => ({
+      ...el,
+      answers: this.shuffle(el.answers)
+    }));
+    for (var i = 0; i < shuffled.length; i++) {
+      for (var j = i + 1; j < shuffled.length; j++) {
+        for (var q = 0; q < 2; q++) {
+          questions.push({
+            title: "Pick the answer that describes you best:",
+            answers: this.shuffle([
+              { text: shuffled[i].answers.pop(), dimension: shuffled[i].name },
+              { text: shuffled[j].answers.pop(), dimension: shuffled[j].name }
+            ])
+          });
+        }
+      }
+    }
+    return this.shuffle(questions);
+  }
+
+  getNextQuestion() {
+    return this.questions[++this.currentQuestion];
+  }
+
+  saveAnswer(question, answerIndex) {
+    if (this.result[question.answers[answerIndex].dimension]) {
+      this.result[question.answers[answerIndex].dimension] += 1;
+    } else {
+      this.result[question.answers[answerIndex].dimension] = 1;
+    }
+  }
 }
 
 export default Assessment;
--- test/assessment.test.js
+++ test/assessment.test.js
@@ -1,25 +1,47 @@
-import Assessment from '../src/assessment'
+import Assessment from "../src/assessment";
 
 // Feel free to rewrite this test suite. This is provided as guidance.
-describe('The Assessment', () => {
-  it('should have 30 questions', () => {
-    expect('this test').toBe('failing');
+describe("The Assessment", () => {
+  let a = new Assessment();
+  it("should have 30 questions", () => {
+    expect(a.questions).toHaveLength(30);
   });
-  it('should not show the same answer twice', () => {
-    expect('this test').toBe('failing');   
+  it("should not show the same answer twice", () => {
+    let answers = a.questions.reduce((acc, cv) => acc.concat(cv.answers), []);
+    let answerSet = new Set(answers);
+    expect(answerSet.size).toBe(60);
   });
-  it('should match each dimension to the other dimensions exactly 2 times', () => {
-    expect('this test').toBe('failing');   
+
+  it("should match each dimension to the other dimensions exactly 2 times", () => {
+    let answerPairs = a.questions
+      .map(q => q.answers.map(a => a.dimension).sort())
+      .sort();
+    console.log(answerPairs);
   });
-  it('should provide ipsative questions (two possible answers)', () => {
-    expect('this test').toBe('failing');   
+
+  it("should provide ipsative questions (two possible answers)", () => {
+    a.questions.forEach(q => expect(q.answers).toHaveLength(2));
+    expect(a.questions.filter(el => el.answers.length !== 2)).toHaveLength(0);
   });
-  describe('when completed', () => {
-    it('should provide the results as an object', () => {
-      expect('this test').toBe('failing');   
+  describe("when completed", () => {
+    let q;
+    while (a.currentQuestion < 29) {
+      q = a.getNextQuestion();
+      a.saveAnswer(q, 0);
+    }
+    it("should provide the results as an object", () => {
+      expect(a.result).toBeInstanceOf(Object);
     });
-    it('should represent the results based on 6 dimensions', () => {
-      expect('this test').toBe('failing');   
+    it("should represent the results based on 6 dimensions", () => {
+      const expected = [
+        "Collaborative",
+        "Integrity",
+        "Result",
+        "Detail",
+        "Adaptive",
+        "Customer"
+      ];
+      expect(Object.keys(a.result)).toEqual(expect.arrayContaining(expected));
     });
   });
 });
